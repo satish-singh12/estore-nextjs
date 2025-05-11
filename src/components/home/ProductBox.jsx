802ac10e0a041492421125b1dd8baf51d2008fba
Satish Singh <satishsingh12@gmail.com>
2025-05-11 12:57:36 +0530

    Recently added product page

--- src/components/home/ProductBox.jsx
+++ src/components/home/ProductBox.jsx
@@ -1,5 +1,24 @@
 import React from "react";
 
-export default function ProductBox() {
-  return <div></div>;
+export default function ProductBox({ products }) {
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {products.map((product) => (
+          <div
+            key={product.id}
+            className="bg-white p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
+          >
+            <img
+              src={product.image}
+              alt={product.title}
+              className="w-full h-48 object-cover mb-4 rounded-lg"
+            />
+            <h2 className="text-lg font-semibold">{product.title}</h2>
+            <p className="text-gray-800 font-bold">${product.price}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
--- src/components/home/RecentlyAdded.jsx
+++ src/components/home/RecentlyAdded.jsx
@@ -1,5 +1,28 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Container from "../Container";
+import ProductBox from "./ProductBox";
 
 export default function RecentlyAdded() {
-  return <div>Recently added</div>;
+  const [products, setProducts] = useState([]);
+  console.log(products);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const data = await response.json();
+    setProducts(data);
+  };
+  return (
+    <Container className="bg-blue-100 mt-5 px-6 py-6">
+      <h1 className="50 font-bold text-2xl text-center mt-4 mb-5">
+        Recently added products
+      </h1>
+      <ProductBox products={products} />
+    </Container>
+  );
 }
